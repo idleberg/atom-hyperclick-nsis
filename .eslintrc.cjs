752e433d1fce749d87ff3c204b8344b0f1cc58ae
idleberg <git@idleberg.com>
2020-10-25 02:40:05 +0200

    change file extension

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "eslint:recommended",
+    "plugin:json/recommended"
+  ],
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    "no-undef": 0,
+    "no-console": 0
+  }
+};
